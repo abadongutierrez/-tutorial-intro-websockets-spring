b1bda92c946f02bc45fbf5607bc6663e3888f4d2
Rafael Gutierrez <abadon.gutierrez@gmail.com>
2022-12-06 14:05:38 -0600

    first commit

---
+++ src/main/java/com/jabaddon/tutorials/introwebsocketsspring/IntroWebsocketsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jabaddon.tutorials.introwebsocketsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroWebsocketsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroWebsocketsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabaddon/tutorials/introwebsocketsspring/IntroWebsocketsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabaddon.tutorials.introwebsocketsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroWebsocketsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
